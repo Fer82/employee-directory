29b7df8e8f2fbe55f5d0c42b54dc483e5eacf513
Fernando Angulo Donoso <fangulo82@gmail.com>
2020-12-30 22:30:15 -0800

    adding src files

---
+++ src/components/Content/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Table from "../Table/";
+import "./style.css";
+
+export default function Content() {
+  return (
+    <React.Fragment>
+      <Table />
+    </React.Fragment>
+  );
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+    </div>
+  )
+}
+
